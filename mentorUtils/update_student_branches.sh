fb6057ee82403c38def9eef5f5d7ef8a19758823
Moonlit Jolteon <moonlit@munebase.dev>
2025-01-15 13:28:11 -0500

    Add utility script to assist in updating student branches

---
+++ mentorUtils/update_student_branches.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+declare -a arr=("ryan" "ronen" "connor" "gideon" "group-dev")
+
+for i in "${arr[@]}"
+do
+    echo "Rebasing $i's branch with code from main"
+    git checkout $i
+    git rebase origin/main
+    git push
+done
