ac4252ea702dff0cfce5ccae8d70994f4954545e
Gideon F. <fitgeraldgideon08@gmail.com>
2025-01-16 20:33:58 -0500

    Basic outline for  Algae manipulator

---
+++ src/main/java/frc/robot/subsystems/AlgaeManipulatorSubsystem.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/**The Subsystem will make use of three motors to intake, manipulate, and output both algae and coral 
+ * the first motor will rotate clockwise and counterclockwise motion to pull in and put out algae:
+ *  I intend to make use of positive and negative output values in order to identify motor direction 
+ * The second motor will rotate either dirrection to pull in and put out algae and coral 
+ * The third motor will swivel to adjust the verticle angle of the entire assembly 
+ */
+
+
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AlgaeManipulatorSubsystem extends SubsystemBase {
+  /** Creates a new AlgaeManipulator. */
+  public AlgaeManipulatorSubsystem() {
+    //motor ID variables
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
+
+
+
+
