efe40fb094cbb29772c40ae24c5a6d9b42a255d5
Moonlit Jolteon <moonlit@munebase.dev>
2025-02-06 15:52:05 -0500

    Streamdeck is responding

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -71,7 +71,7 @@ public class RobotContainer {
     initializeClimberSubsystem();
     initializeElevatorSubsystem();
     initializeAlgaeManipulatorSubsystem();
-    // initializeStreamdeckBasedControls();
+    initializeStreamdeckBasedControls();
 
     // initializeExampleSubsystem();
     initializeMultisystemCommands();
@@ -132,7 +132,6 @@ public class RobotContainer {
     this.swerveSubsystem.setDefaultCommand(driveFieldOrientedAngularVelocity);
   }
 
-  @SuppressWarnings("unused")
   private void initializeStreamdeckBasedControls() {
     SwerveInputStream driveAngularVelocity = SwerveInputStream.of(swerveSubsystem.getSwerveDrive(),
       () -> operatorStreamdeck.getYShiftSpeed(),
@@ -143,6 +142,7 @@ public class RobotContainer {
 
     Command operatorShifting = swerveSubsystem.drive(driveAngularVelocity);
     this.operatorStreamdeck.shifting.whileTrue(operatorShifting);
+    this.operatorStreamdeck.reefOne.onChange(this.algaeManipulatorSubsystem.runOnce(() -> System.out.println("Reef One Status Changed")));
 
     this.operatorStreamdeck.algaeIntake.whileTrue(this.algaeManipulatorSubsystem.run(() -> this.algaeManipulatorSubsystem.intakeAlgae(0.5)));
     this.operatorStreamdeck.algaeOuttake.whileTrue(this.algaeManipulatorSubsystem.run(() -> this.algaeManipulatorSubsystem.outtakeAlgae(0.5)));
